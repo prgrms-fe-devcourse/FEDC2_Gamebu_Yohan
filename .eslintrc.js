2481416f1abb1c5dbffb5a3bd605848ab7010918
feel0321 <dishate@naver.com>
2022-06-06 22:42:44 +0900

    chore: eslint와 vscode prettier 초기 설정을 완료했다 .vscode로 기본 저장 시 prettier 기본 설정으로 저장한다 eslint airbnb설정으로 문법 오류, 안쓰는 변수 등에 대해서 경고한다

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', 'jsx'] }],
+    'linebreak-style': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+};
