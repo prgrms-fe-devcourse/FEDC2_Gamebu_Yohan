105c27de74f7ae2ed8fa7e0ec3de75b4d639bfb3
JoChaeWoo <jcy6343@naver.com>
2022-06-09 20:06:53 +0900

    chore: 파일 의존성과 추가 설정을 수정했다. Icon 컴포넌트에 필요한 Buffer 모듈을 의존성에 추가하고 스토리북과 호환되지 않는 alias 설정을 제거했다.

--- craco.config.js
+++ craco.config.js
@@ -4,12 +4,4 @@ module.exports = {
   babel: {
     presets: ['@emotion/babel-preset-css-prop'],
   },
-  webpack: {
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@contexts': path.resolve(__dirname, 'src/contexts'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-    },
-  },
 };
