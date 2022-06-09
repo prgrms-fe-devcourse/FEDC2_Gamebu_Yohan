18a8715a9c71906120b1ac51b3af0e5fc4a59db6
JoChaeWoo <jcy6343@naver.com>
2022-06-09 20:08:42 +0900

    feat: Icon 컴포넌트와 스토리북 파일을 추가했다. feather-icons을 컴포넌트로 사용할 수 있는 Icon 컴포넌트를 추가했다. 일부 파일을 바뀐 설정에 맞게 수정했다.

--- src/hooks/useAsync.js
+++ src/hooks/useAsync.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import useAsyncFn from '@hooks/useAsyncFn';
+import useAsyncFn from './useAsyncFn';
 
 const useAsync = (fn, deps) => {
   const [state, callback] = useAsyncFn(fn, deps);
--- src/stories/Image.stories.js
+++ src/stories/Image.stories.js
@@ -1,4 +1,4 @@
-import Image from '@components/Image';
+import Image from '../components/Image';
 
 export default {
   title: 'Component/Image',
