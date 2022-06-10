d8967b6c246bee6d5d9636c18cdd83c4a6be4f20
JoChaeWoo <jcy6343@naver.com>
2022-06-10 17:06:16 +0900

    feat: API 관련 함수를 추가했다. API END POINT 중복사용을 막기위해서 별도의 함수(fetch)를 추가하고, 인증이 필요한 경우 사용할 수 있는 별도의 함수 (authFetch) 함수를 추가했다.

---
+++ src/util/fetch.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const API_END_POINT = process.env.REACT_APP_API_END_POINT;
+
+export const fetch = async (url, options) => {
+  try {
+    const result = await axios(`${API_END_POINT}${url}`, options);
+    return result.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const authFetch = async (url, options) => {
+  // TODO: 저장 방식 정한 뒤 수정하기.
+  const token = window.localStorage.getItem('token');
+  try {
+    const result = await axios(`${API_END_POINT}${url}`, {
+      ...options,
+      Authorization: `bearer ${token}`,
+    });
+    return result.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const temp = 0;
