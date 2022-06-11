cda208d6b8c2bab08480ba609a25a8ba4378a66e
JoChaeWoo <jcy6343@naver.com>
2022-06-11 23:31:52 +0900

    feat: search page의 기본 구조를 구현했다. search page의 구현에 필수적인 input과 input 값에 따른 api 호출 이벤트를 설정했다.

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import useAsyncFn from '@hooks/useAsyncFn';
+import { searchAll } from '@utils/search';
+import React, { useEffect, useRef, useState } from 'react';
 
 function SearchPage() {
-  return <div>SearchPage</div>;
+  const inputRef = useRef(null);
+  const [userList, setUserList] = useState([]);
+  const [postList, setPostList] = useState([]);
+  const [searchResult, asyncSearchAll] = useAsyncFn(
+    async (keyword) => {
+      return searchAll(keyword);
+    },
+    [inputRef]
+  );
+  const handleKeyDown = ({ key }) => {
+    if (key === 'Enter' && inputRef.current.value) {
+      asyncSearchAll(inputRef.current.value);
+    }
+  };
+  const classify = (datas) => {
+    if (!datas) return;
+    setUserList(datas.filter((data) => data.fullName));
+    setPostList(datas.filter((data) => data.title));
+  };
+  useEffect(() => {
+    const { isLoading, value } = searchResult;
+    if (!isLoading) {
+      classify(value);
+    }
+  }, [searchResult]);
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
+      <div>
+        <input ref={inputRef} onKeyDown={handleKeyDown} />
+      </div>
+      <ul>
+        {userList.map(({ fullName, email, _id }) => (
+          <li key={_id}>
+            <div>{fullName}</div>
+            <div>{email}</div>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {postList.map(({ title, createdAt, _id }) => (
+          <li key={_id}>
+            <div>{title}</div>
+            <div>{createdAt}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default SearchPage;
