7f237ac6db3c91578851ad2e42506f5261235dfd
feel0321 <dishate@naver.com>
2022-06-12 15:12:40 +0900

    feat: 로그인 페이지에 헤더를 추가했다

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const ContentWrapper = styled.div`
+  padding: 1.5rem;
+`;
+
+const LoginHeader = styled.h1`
+  font-size: 2rem;
+  text-align: center;
+  padding: 1.5rem;
+`;
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <ContentWrapper>
+      <LoginHeader>Login</LoginHeader>
+    </ContentWrapper>
+  );
 }
 
 export default LoginPage;
