5ccd85a4e039d24739834d7333f6e6aea49ab4eb
mieumje <66072832+mieumje@users.noreply.github.com>
2022-06-13 19:48:15 +0900

    feat: 메인 홈 페이지 기본 레이아웃을 구성했다.
    Slider 배너의 경우 더미 데이터를 통해 Slider 동작을 확인했다.
    
    최신 글 Container에서 Title이 긴 경우에 대한 확인과
    Posts를 불러와 title과 comments를 화면에 렌더링하도록 설정했다.
    
    현재 API 상 전체 Posts를 불러오고 있지 못해 특정 채널의 Posts를 불러와 처리했다.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,141 @@
-import React from 'react';
+import styled from '@emotion/styled';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Header from '@components/Header';
+import Divider from '@components/Divider';
+import { COLOR_MAIN, COLOR_SIGNATURE } from '@utils/color';
+import { fetch } from '@utils/fetch';
+import useAsync from '@hooks/useAsync';
+
+const HomePageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const RecentPostsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SliderWrapper = styled.div`
+  margin-bottom: 2rem;
+`;
+
+const SliderItemWrapper = styled.div`
+  width: 100%;
+  height: 10rem;
+  background-color: #ffa;
+  border-radius: 0.4rem;
+  display: flex;
+  align-items: center;
+`;
+
+const RecentPostsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 2rem;
+  padding: 8px 0;
+`;
+
+const PostCategory = styled.div`
+  font-size: 0.5rem;
+  color: ${COLOR_MAIN};
+  margin-right: 0.5rem;
+`;
+
+const PostTitle = styled.div`
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 2rem;
+  color: ${COLOR_SIGNATURE};
+  max-width: 10rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  margin-right: 0.2rem;
+`;
+
+const PostComments = styled.div`
+  font-size: 0.8rem;
+  line-height: 2rem;
+  color: red;
+`;
+
+const settings = {
+  dots: false, // 슬라이드 밑에 점 보이게
+  infinite: true, // 무한으로 반복
+  // speed: 500,     // 넘기는 속도
+  autoplay: true, // 자동으로 넘김
+  autoplaySpeed: 1500, // 자동으로 넘어가는 속도
+  slidesToShow: 1, // 스크린에 보여지는 슬라이드 개수
+  slidesToScroll: 1, // n장씩 뒤로 넘어가게 함
+  centerMode: true, // 중앙정렬
+  centerPadding: '0px', // 0px 하면 슬라이드 끝쪽 이미지가 안잘림
+};
 
 function HomePage() {
-  return <div>HomePage</div>;
+  const url = 'posts/channel/629f0c7c7e01ad1cb7250151';
+
+  const getPosts = useAsync(async () => {
+    const result = await fetch(url);
+    return result;
+  }, []);
+
+  return (
+    <HomePageContainer>
+      <SliderWrapper>
+        <Header strong>게임 카테고리</Header>
+        <Divider />
+        <Slider {...settings}>
+          <SliderItemWrapper>
+            <Header level={2}>1 item</Header>
+          </SliderItemWrapper>
+          <SliderItemWrapper>
+            <Header level={2}>2 item</Header>
+          </SliderItemWrapper>
+          <SliderItemWrapper>
+            <Header level={2}>3 item</Header>
+          </SliderItemWrapper>
+          <SliderItemWrapper>
+            <Header level={2}>4 item</Header>
+          </SliderItemWrapper>
+          <SliderItemWrapper>
+            <Header level={2}>5 item</Header>
+          </SliderItemWrapper>
+          <SliderItemWrapper>
+            <Header level={2}>6 item</Header>
+          </SliderItemWrapper>
+        </Slider>
+      </SliderWrapper>
+      <RecentPostsContainer>
+        <Header strong>최신 글</Header>
+        <Divider />
+        <RecentPostsWrapper>
+          <PostCategory>Category</PostCategory>
+          <PostTitle>Too Long Title Here Recent Posts 1</PostTitle>
+          <PostComments>[2]</PostComments>
+        </RecentPostsWrapper>
+        <RecentPostsWrapper>
+          <PostCategory>Category</PostCategory>
+          <PostTitle>Too Long Title Here Recent Posts 2</PostTitle>
+          <PostComments>[2]</PostComments>
+        </RecentPostsWrapper>
+        {getPosts.value &&
+          getPosts.value.map((item) => {
+            return (
+              <RecentPostsWrapper key={item._id}>
+                <PostCategory>Category</PostCategory>
+                <PostTitle>{item.title}</PostTitle>
+                <PostComments>[{item.comments.length}]</PostComments>
+              </RecentPostsWrapper>
+            );
+          })}
+      </RecentPostsContainer>
+    </HomePageContainer>
+  );
 }
 
 export default HomePage;
