4050283fa341dab6a8343243e9b8c2a80ec9445a
mieumje <66072832+mieumje@users.noreply.github.com>
2022-06-14 21:59:01 +0900

    feat: 카테고리 별 게임명을 작성하기 위한 컴포넌트를 생성하고 적용했다.

--- src/pages/CategoriesPage.js
+++ src/pages/CategoriesPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { COLOR_SIGNATURE } from '@utils/color';
 import maple from '../assets/img/maple.png';
 import lol from '../assets/img/lol.png';
 import lostark from '../assets/img/lostark.png';
@@ -19,7 +20,6 @@ const CATEGORIES_CONTAINER = styled.div`
   display: grid;
   grid-template-rows: repeat(3, 1fr);
   grid-template-columns: repeat(3, 1fr);
-  border: 1px solid blue;
   gap: 1rem;
 `;
 
@@ -29,6 +29,14 @@ const STYLED_IMG = styled.img`
   height: 100px;
 `;
 
+const GAME_TITLE = styled.div`
+  text-align: center;
+  font-size: 0.75rem;
+  font-weight: 700;
+  color: ${COLOR_SIGNATURE};
+  margin-top: 0.1rem;
+`;
+
 function CategoriesPage() {
   return (
     <>
@@ -36,23 +44,23 @@ function CategoriesPage() {
       <CATEGORIES_CONTAINER>
         <GAME_ITEM>
           <STYLED_IMG src={maple} />
-          <span>title here</span>
+          <GAME_TITLE>메이플스토리</GAME_TITLE>
         </GAME_ITEM>
         <GAME_ITEM>
           <STYLED_IMG src={lol} />
-          <span>title here</span>
+          <GAME_TITLE>리그오브레전드</GAME_TITLE>
         </GAME_ITEM>
         <GAME_ITEM>
           <STYLED_IMG src={lostark} />
-          <span>title here</span>
+          <GAME_TITLE>로스트아크</GAME_TITLE>
         </GAME_ITEM>
         <GAME_ITEM>
           <STYLED_IMG src={overwatch} />
-          <span>title here</span>
+          <GAME_TITLE>오버워치</GAME_TITLE>
         </GAME_ITEM>
         <GAME_ITEM>
           <STYLED_IMG src={battleground} />
-          <span>title here</span>
+          <GAME_TITLE>배틀그라운드</GAME_TITLE>
         </GAME_ITEM>
       </CATEGORIES_CONTAINER>
     </>
