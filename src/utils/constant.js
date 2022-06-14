07be6397c0888b224c25d919f91d49c6c16aeabe
mieumje <66072832+mieumje@users.noreply.github.com>
2022-06-14 14:52:23 +0900

    feat: 게임 카테고리 채널의 id를 별도의 상수로 분류했다.
    추후에 카테고리가 더 추가되면 업데이트를 해주어야 한다.

---
+++ src/utils/constant.js
@@ -0,0 +1,25 @@
+// TODO: 추후 채널 카테고리 더 추가
+const CHANNELS = [
+  {
+    id: '62a7367f5517e27ffcab3bcb',
+    name: '메이플스토리',
+  },
+  {
+    id: '62a736925517e27ffcab3bcf',
+    name: '리그오브레전드',
+  },
+  {
+    id: '62a736a15517e27ffcab3bd5',
+    name: '배틀그라운드',
+  },
+  {
+    id: '62a818db5517e27ffcab3ce2',
+    name: '로스트아크',
+  },
+  {
+    id: '62a818e85517e27ffcab3ce6',
+    name: '오버워치',
+  },
+];
+
+export default CHANNELS;
