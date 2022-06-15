ebec41fa9034f8812496181c8d6d4423f8ca56be
sangjin149 <sangjin149@gmail.com>
2022-06-15 14:13:49 +0900

    chore: fetch pull

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -8,19 +8,30 @@ export const fetch = async (url, options) => {
     return result.data;
   } catch (error) {
     console.error(error);
+    return error;
   }
 };
 
 export const authFetch = async (url, options) => {
-  // TODO: 저장 방식 정한 뒤 수정하기.
-  const token = window.localStorage.getItem('token');
   try {
-    const result = await axios(`${API_END_POINT}${url}`, {
-      ...options,
-      Authorization: `bearer ${token}`,
-    });
-    return result.data;
+    const token = document.cookie
+      ?.split(';')
+      ?.find((cookies) => cookies.includes('GAMEBU_TOKEN'))
+      ?.slice(13);
+
+    if (token) {
+      const result = await axios(`${API_END_POINT}${url}`, {
+        ...options,
+        headers: {
+          Authorization: `bearer ${token}`,
+        },
+      });
+      return result.data;
+    }
+
+    throw new Error('token is not found in cookie');
   } catch (error) {
     console.error(error);
+    return error;
   }
 };
