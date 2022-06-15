1430517b56d67c8c7ac4556ad49689de3712021b
960817 <92906766+960817@users.noreply.github.com>
2022-06-16 04:09:10 +0900

    refactor: post detail page의 path 를 수정했다.

--- src/components/Channels/ChannelImageContainer.js
+++ src/components/Channels/ChannelImageContainer.js
@@ -8,60 +8,6 @@ import { authFetch, fetch } from '@utils/fetch';
 import axios from 'axios';
 
 function ChannelImageContainer({ url }) {
-  const testFullName = {
-    id: 2,
-    first: 'Ko',
-    last: 'GwangFeel',
-  };
-  const form = new FormData();
-  form.append(
-    'title',
-    JSON.stringify({
-      dt: '칼바람 할래요',
-      tg: ['칼바람', '초보환영'],
-      dd: 'abcd <-- 친추주세영',
-    })
-  );
-  form.append('image', null);
-  form.append('channelId', '62a817a85517e27ffcab3cce');
-
-  const DONG_TOKEN =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYyOWYwN2ZhN2UwMWFkMWNiNzI1MDEzMSIsImVtYWlsIjoiZG9uZ2VvbiJ9LCJpYXQiOjE2NTUxOTM5NTF9.GtiQO30CJg2VQQT4CCKOz3yZYjwzEw66aqlwe-ZC8GQ';
-
-  const pushForm = async () => {
-    const res = await authFetch('posts/create', {
-      method: 'POST',
-      'Content-Type': 'multipart/form-data',
-      data: form,
-    });
-    console.log(res);
-  };
-
-  const checkPost = async () => {
-    const res = await fetch('posts/62a974694f54343f5dea4d12');
-    console.log(res);
-    console.log(res.title);
-    console.log(JSON.parse(res.title).dt);
-  };
-
-  // const test = async () => {
-  //   const response = await fetch('search/all/feel');
-  //   console.log('response[0]');
-  //   console.log(response[0]);
-  //   console.log('response[0].fullName');
-  //   console.log(response[0].fullName);
-  //   console.log('JSON.parse(response[0].fullName)');
-  //   console.log(JSON.parse(response[0].fullName));
-  // };
-
-  useEffect(() => {
-    // pushData();
-    // pushForm();
-    // checkPost();
-    // test();
-  }, []);
-
-  // console.log(url);
   return (
     <Image
       width="100%"
