bf2a7d0cad709effc58aa8cd881bd664e2564f19
mieumje <66072832+mieumje@users.noreply.github.com>
2022-06-16 15:19:59 +0900

    feat: 즐겨찾기 활성화를 위한 Start Icon을 추가했다.

--- src/pages/CategoriesPage.js
+++ src/pages/CategoriesPage.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from '@emotion/styled';
+import ContextProvider from '@contexts/ContextProvider';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { COLOR_MAIN, COLOR_SIGNATURE } from '@utils/color';
 import Header from '@components/Header';
 import Divider from '@components/Divider';
@@ -10,8 +13,10 @@ import lol from '@assets/img/lol.png';
 import lostark from '@assets/img/lostark.png';
 import overwatch from '@assets/img/overwatch.png';
 import battleground from '@assets/img/battleground.png';
+import useValueContext from '@hooks/useValueContext';
 
 const GAME_ITEM = styled.div`
+  position: relative;
   width: 100%;
   height: 100px;
   border: 1px solid ${COLOR_MAIN};
@@ -35,6 +40,13 @@ const STYLED_IMG = styled.img`
   height: 100px;
 `;
 
+const ICON_WRAPPER = styled.div`
+  position: absolute;
+  z-index: 1000;
+  top: 0;
+  right: 0;
+`;
+
 const GAME_TITLE = styled.div`
   text-align: center;
   font-size: 0.75rem;
@@ -44,11 +56,16 @@ const GAME_TITLE = styled.div`
 `;
 
 function CategoriesPage() {
+  const { isLogin, user } = useValueContext();
   const [channels] = useState(CHANNELS);
   const [images] = useState([maple, lol, battleground, lostark, overwatch]);
 
   return (
     <>
+      <ContextProvider>
+        <div>ContextProvider : {user && user.username}</div>
+        {console.log(user)}
+      </ContextProvider>
       <Header strong>게임 카테고리</Header>
       <Divider />
       <CATEGORIES_CONTAINER>
@@ -58,6 +75,9 @@ function CategoriesPage() {
               <Link to={`/channel/${channel.id}`} key={`${channel.id}`}>
                 <GAME_ITEM>
                   <STYLED_IMG src={images[index]} />
+                  <ICON_WRAPPER>
+                    <StarBorderIcon fontSize="small" sx={{ color: 'red' }} />
+                  </ICON_WRAPPER>
                 </GAME_ITEM>
                 <GAME_TITLE>{`${channel.name}`}</GAME_TITLE>
               </Link>
