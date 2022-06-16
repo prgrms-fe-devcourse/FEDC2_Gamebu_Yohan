51b739216c1261b4b7b12838e8305a4addb98c2f
JoChaeWoo <jcy6343@naver.com>
2022-06-16 17:07:30 +0900

    refactor: 컴포넌트를 분리했다. SearchPage 내부에서 반복되는 컴포넌트를 분리해서 적용했다.

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -6,20 +6,16 @@ import { Button, IconButton, TextField } from '@mui/material';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import { searchAll } from '@utils/search';
 import React, { useEffect, useRef, useState } from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
+import { COLOR_SIGNATURE } from '@utils/color';
+import List from '@components/List';
 
-const List = styled.ul`
-  overflow-y: auto;
-  & > li {
-    margin-top: 4px;
-  }
-`;
-const ListWrapper = styled.div`
+const HeaderBox = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
 `;
-const ListContainer = styled.div`
+const ListWrapper = styled.div`
   flex-grow: 1;
 `;
 const HeaderWrapper = styled.div`
@@ -27,15 +23,19 @@ const HeaderWrapper = styled.div`
   justify-content: space-between;
   align-items: center;
 `;
-const PageWrapper = styled.div`
+const Container = styled.div`
   height: 100%;
   display: grid;
   grid-template-rows: 5rem 1fr 1fr;
   grid-template-columns: 100%;
+  justify-items: center;
+`;
+const RouteLink = styled(Link)`
+  text-decoration: none;
+  color: ${COLOR_SIGNATURE};
 `;
 function SearchPage() {
   const inputRef = useRef(null);
-  const navigate = useNavigate();
   const [searchParam, setSearchParam] = useSearchParams();
   const [inputError, setInputError] = useState({
     error: false,
@@ -98,82 +98,75 @@ function SearchPage() {
     }
   }, [searchResult]);
 
+  const InputProps = {
+    endAdornment: (
+      <IconButton onClick={handleSearchClick}>
+        <SearchRoundedIcon fontSize="large" aria-label="search" />
+      </IconButton>
+    ),
+    style: { fontSize: 20 },
+  };
+
   return (
-    <PageWrapper>
+    <Container>
       <TextField
         fullWidth
-        InputProps={{
-          endAdornment: (
-            <IconButton onClick={handleSearchClick}>
-              <SearchRoundedIcon fontSize="large" aria-label="search" />
-            </IconButton>
-          ),
-          style: { fontSize: 20 },
-        }}
+        InputProps={InputProps}
         inputRef={inputRef}
         onKeyDown={handleKeyDown}
         {...inputError}
       />
-      <ListWrapper>
-        <HeaderWrapper>
-          <Header level={1} strong>
-            사용자
-          </Header>
-          <Button
-            onClick={() => {
-              navigate(`/search/user?k=${searchParam.get('k')}`);
-            }}
-            variant="text"
-            color="primary"
-          >
-            모두 보기
-          </Button>
-        </HeaderWrapper>
-
-        <ListContainer>
-          {userList.length === 0 ? (
-            <Header>키워드에 해당하는 결과가 없습니다</Header>
-          ) : (
-            <List>
-              {userList.slice(0, 3).map((user) => (
-                <li key={user._id}>
-                  <Card.User>{user}</Card.User>
-                </li>
-              ))}
-            </List>
-          )}
-        </ListContainer>
-      </ListWrapper>
-      <ListWrapper>
-        <HeaderWrapper>
-          <Header level={1} strong>
-            포스트
-          </Header>
-          <Button
-            onClick={() => {
-              navigate(`/search/post?k=${searchParam.get('k')}`);
-            }}
-            variant="text"
-            color="primary"
-          >
-            모두 보기
-          </Button>
-        </HeaderWrapper>
-        <ListContainer>
-          {postList.length === 0 ? (
-            <Header>키워드에 해당하는 결과가 없습니다</Header>
-          ) : (
-            <List>
-              {postList.slice(0, 3).map((post) => (
-                <li key={post._id}>
-                  <Card.Post>{post}</Card.Post>
-                </li>
-              ))}
-            </List>
-          )}
-        </ListContainer>
-      </ListWrapper>
-    </PageWrapper>
+      {searchParam.get('k') ? (
+        <>
+          <HeaderBox>
+            <HeaderWrapper>
+              <Header level={1} strong>
+                사용자
+              </Header>
+              <Button variant="text" color="primary">
+                <RouteLink to={`/search/user?k=${searchParam.get('k')}`}>
+                  모두 보기
+                </RouteLink>
+              </Button>
+            </HeaderWrapper>
+            <ListWrapper>
+              <List
+                items={userList}
+                keyName="_id"
+                alt="키워드에 해당하는 결과가 없습니다"
+                limit={3}
+                gap="8px"
+                frame={<Card.User />}
+              />
+            </ListWrapper>
+          </HeaderBox>
+          <HeaderBox>
+            <HeaderWrapper>
+              <Header level={1} strong>
+                포스트
+              </Header>
+              <Button variant="text" color="primary">
+                <RouteLink to={`/search/post?k=${searchParam.get('k')}`}>
+                  모두 보기
+                </RouteLink>
+              </Button>
+            </HeaderWrapper>
+            <ListWrapper>
+              <List
+                items={postList}
+                keyName="_id"
+                alt="키워드에 해당하는 결과가 없습니다"
+                limit={3}
+                gap="8px"
+                frame={<Card.Post />}
+              />
+            </ListWrapper>
+          </HeaderBox>
+        </>
+      ) : (
+        <Header margin="10% 0">검색어를 입력해 파티를 찾아보세요!</Header>
+      )}
+    </Container>
   );
 }
 
