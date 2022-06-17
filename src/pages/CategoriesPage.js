4a413244b52e43322b5492f15cb64ed477ae8317
mieumje <66072832+mieumje@users.noreply.github.com>
2022-06-17 21:10:18 +0900

    refactor: 즐겨찾기 삭제 로직을 빈 배열을 넘기는 구조로 다시 수정했다.
    즐겨찾기 목록이 없는 경우 빈 배열을 stringify해서 보내기로 다시 수정했다.

--- src/pages/CategoriesPage.js
+++ src/pages/CategoriesPage.js
@@ -7,10 +7,14 @@ import { COLOR_MAIN, COLOR_SIGNATURE } from '@utils/color';
 import Header from '@components/Header';
 import Divider from '@components/Divider';
 import { CATEGORIES, CHANNELS } from '@utils/constants';
+import maple from '@assets/img/maple.png';
+import lol from '@assets/img/lol.png';
+import lostark from '@assets/img/lostark.png';
+import overwatch from '@assets/img/overwatch.png';
+import battleground from '@assets/img/battleground.png';
 import useValueContext from '@hooks/useValueContext';
 import { authFetch } from '@utils/fetch';
 import useActionContext from '@hooks/useActionContext';
-import { maple, lol, lostark, overwatch, battleground } from '@assets/img';
 import {
   GameIcon,
   GameImage,
@@ -20,6 +24,7 @@ import {
 
 const GameItem = styled.div`
   position: relative;
+
   width: 100%;
   height: 100px;
   border: 1px solid ${COLOR_MAIN};
@@ -28,7 +33,6 @@ const GameItem = styled.div`
   overflow: hidden;
   grid-template-rows: repeat(2, 1fr);
 `;
-
 const CategoriesContainer = styled.div`
   width: 100%;
   display: grid;
@@ -36,14 +40,12 @@ const CategoriesContainer = styled.div`
   grid-template-columns: repeat(3, 1fr);
   gap: 1rem;
 `;
-
 const MessageContainer = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
   padding: 1rem 0;
 `;
-
 function CategoriesPage() {
   const { user } = useValueContext();
   const { favorites } = useActionContext();
@@ -56,13 +58,11 @@ function CategoriesPage() {
     '62a818db5517e27ffcab3ce2': lostark,
     '62a818e85517e27ffcab3ce6': overwatch,
   });
-
   useEffect(() => {
     if (user && user.username) {
       setUserFavorites(JSON.parse(user.username));
     }
   }, [user]);
-
   const updateFavorites = useCallback(
     async (e, id, name) => {
       e.preventDefault();
@@ -70,19 +70,15 @@ function CategoriesPage() {
       if (user.username) {
         likes = JSON.parse(user.username);
       }
-
       if (likes.includes(id)) {
         alert('이미 추가된 채널입니다.');
         // TODO : Toast 또는 다른 UI로직
         return;
       }
-
       likes.push(id);
       likes.sort();
       setUserFavorites([...userFavorites, likes]);
-
       alert(`즐겨찾기에 ${name} 채널을 추가`);
-
       const res = await authFetch('settings/update-user', {
         method: 'PUT',
         data: {
@@ -94,47 +90,30 @@ function CategoriesPage() {
     },
     [userFavorites, favorites, user]
   );
-
   const deleteFavorites = useCallback(
     async (e, id) => {
       e.preventDefault();
-
       alert(`즐겨찾기에서 ${CATEGORIES[id]} 채널을 삭제`);
-      const newFavorites = userFavorites.filter(
-        (item) => item !== id && item !== ''
-      );
-      const favoritesData =
-        newFavorites.length < 1
-          ? JSON.stringify([''])
-          : JSON.stringify(newFavorites);
-
+      const newFavorites = userFavorites.filter((item) => item !== id);
       setUserFavorites(newFavorites);
       const res = await authFetch('settings/update-user', {
         method: 'PUT',
         data: {
-          username: favoritesData,
+          username: JSON.stringify(newFavorites),
         },
       });
       favorites(res);
     },
     [userFavorites, favorites]
   );
-
   return (
     <>
       <ContextProvider>
         <Header strong>즐겨찾기 목록</Header>
         <Divider />
-        {userFavorites.length === 1 && userFavorites[0] === '' ? (
-          <MessageContainer>
-            <MessageTitle color={COLOR_MAIN} weight={400}>
-              즐겨찾기를 등록해보세요.
-            </MessageTitle>
-          </MessageContainer>
-        ) : (
+        {userFavorites.length > 0 ? (
           <CategoriesContainer>
             {userFavorites.map((item) => {
-              if (item === '') return;
               return (
                 <Link to={`/channel/${item}`} key={`${item}`}>
                   <GameItem>
@@ -151,6 +130,12 @@ function CategoriesPage() {
               );
             })}
           </CategoriesContainer>
+        ) : (
+          <MessageContainer>
+            <MessageTitle color={COLOR_MAIN} weight={400}>
+              즐겨찾기를 등록해보세요.
+            </MessageTitle>
+          </MessageContainer>
         )}
       </ContextProvider>
       <Header strong>게임 카테고리</Header>
@@ -181,5 +166,4 @@ function CategoriesPage() {
     </>
   );
 }
-
 export default CategoriesPage;
