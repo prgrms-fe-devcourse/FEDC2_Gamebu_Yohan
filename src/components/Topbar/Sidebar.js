c720175581ef58364132f738d042d1b085fccfec
mieumje <66072832+mieumje@users.noreply.github.com>
2022-06-17 21:09:05 +0900

    feat: Sidebar에 즐겨찾기 항목과 채널 카테고리 목록을 추가했다.

--- src/components/Topbar/Sidebar.js
+++ src/components/Topbar/Sidebar.js
@@ -8,10 +8,16 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
+import GamesIcon from '@mui/icons-material/Games';
+import StarRateIcon from '@mui/icons-material/StarRate';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import { Link } from 'react-router-dom';
+import { CATEGORIES, CHANNELS } from '@utils/constants';
+import { useState, useEffect } from 'react';
+import useValueContext from '@hooks/useValueContext';
+import Header from '@components/Header';
 
 const DrawerHeader = styled.div`
   display: 'flex';
@@ -19,12 +25,28 @@ const DrawerHeader = styled.div`
   justify-content: 'flex-end';
 `;
 
+const HeaderWrapper = styled.div`
+  padding: 0.5rem;
+  margin-bottom: 0.5rem;
+`;
+
 function Sidebar({ open, onClose }) {
+  const { user } = useValueContext();
+  const [userFavorites, setUserFavorites] = useState([]);
+  const [channels] = useState(CHANNELS);
+
+  useEffect(() => {
+    if (user && user.username) {
+      setUserFavorites(JSON.parse(user.username));
+    }
+  }, [user]);
+  console.log(user, userFavorites);
   return (
     <Drawer variant="temporary" open={open} onClose={onClose}>
       <DrawerHeader>
         <IconButton onClick={onClose}>
           <ChevronLeftIcon />
+          <Header>Gamebu</Header>
         </IconButton>
       </DrawerHeader>
       <Divider />
@@ -34,18 +56,39 @@ function Sidebar({ open, onClose }) {
             <ListItemText primary={<Link to="/login">로그인</Link>} />
           </ListItemButton>
         </ListItem>
-        {['채널1 RPG', '채널2 FPS', '채널3 자유', '채널4 홍보'].map(
-          (text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          )
-        )}
+        <Divider />
+        <HeaderWrapper>
+          <Header>즐겨찾기 목록</Header>
+        </HeaderWrapper>
+        {user &&
+          userFavorites.map((item) => {
+            if (item === '') return;
+
+            return (
+              <ListItem key={item} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <StarRateIcon sx={{ color: '#f2f20d' }} />
+                  </ListItemIcon>
+                  <ListItemText primary={CATEGORIES[item]} />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        <Divider />
+        <HeaderWrapper>
+          <Header>채널 카테고리</Header>
+        </HeaderWrapper>
+        {channels.map((item) => (
+          <ListItem key={item.id} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <GamesIcon />
+              </ListItemIcon>
+              <ListItemText primary={item.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
       </List>
     </Drawer>
   );
