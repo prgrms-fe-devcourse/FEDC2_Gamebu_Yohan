51ff5bae5bd3a72cf9dc89eb77453e27783cd26b
JoChaeWoo <jcy6343@naver.com>
2022-06-17 16:00:56 +0900

    feat: 라우팅 방식을 수정했다. SearchAllpage에서 사용하는 input과 검색어 처리 로직들이 이후 SearchUserPage, SearchPostPage와 중복되기 때문에 nested route를 사용해 중복되는 부분을 SearchPage로 분리했다.

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ import SearchPage from '@pages/SearchPage';
 import SearchPostPage from '@pages/SearchPostPage';
 import SearchUserPage from '@pages/SearchUserPage';
 import SignupPage from '@pages/SignupPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ContextProvider from '@contexts/ContextProvider';
 import InterestedChannelModal from '@components/InterestedChannelModal';
+import SearchAllPage from '@pages/SearchAllPage';
 
 function App() {
   return (
@@ -25,9 +26,12 @@ function App() {
           <Route path="/" element={<Navbar />}>
             <Route index element={<HomePage />} />
             <Route path="alram/:user" element={<AlramPage />} />
-            <Route path="search/all" element={<SearchPage />} />
-            <Route path="search/user" element={<SearchUserPage />} />
-            <Route path="search/post" element={<SearchPostPage />} />
+            <Route path="search" element={<SearchPage />}>
+              <Route path="" element={<Navigate to="all" replace />} />
+              <Route path="all" element={<SearchAllPage />} />
+              <Route path="user" element={<SearchUserPage />} />
+              <Route path="post" element={<SearchPostPage />} />
+            </Route>
             <Route path="channel/:channelId" element={<ChannelPage />} />
             <Route path="channel/categories" element={<CategoriesPage />} />
             <Route path="posts/write" element={<PostWritePage />} />
