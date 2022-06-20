90461625c8c527e6bfe67cc55919fbe0d2e963a7
mieumje <66072832+mieumje@users.noreply.github.com>
2022-06-20 17:39:43 +0900

    style: 배너 setting에 대한 주석을 삭제했다.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -72,15 +72,14 @@ const PostComments = styled.div`
 `;
 
 const settings = {
-  dots: false, // 슬라이드 밑에 점 보이게
-  infinite: true, // 무한으로 반복
-  // speed: 500,     // 넘기는 속도
-  autoplay: true, // 자동으로 넘김
-  autoplaySpeed: 1500, // 자동으로 넘어가는 속도
-  slidesToShow: 1, // 스크린에 보여지는 슬라이드 개수
-  slidesToScroll: 1, // n장씩 뒤로 넘어가게 함
-  centerMode: true, // 중앙정렬
-  centerPadding: '0px', // 0px 하면 슬라이드 끝쪽 이미지가 안잘림
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: '0px',
 };
 
 function HomePage() {
