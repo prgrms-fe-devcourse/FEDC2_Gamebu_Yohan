137e832de27a966501ef2a30dbec8475dd573c74
sangjin149 <sangjin149@gmail.com>
2022-06-21 19:28:01 +0900

    chore/feat: 채널 배너, 채널 아이콘, 태그 아이콘을 저장했다.

---
+++ src/assets/TagAvatars/index.js
@@ -0,0 +1,24 @@
+import competition from './Competition.png';
+import dealer from './Dealer.png';
+import duo from './Duo.png';
+import fps from './FPS.png';
+import healer from './Healer.png';
+import party from './Party.png';
+import raid from './Raid.png';
+import rpg from './RPG.png';
+import support from './Support.png';
+import tanker from './Tanker.png';
+
+const TagAvatars = {
+  경쟁: competition,
+  딜러: dealer,
+  듀오: duo,
+  FPS: fps,
+  힐러: healer,
+  파티: party,
+  레이드: raid,
+  RPG: rpg,
+  서폿: support,
+  탱커: tanker,
+};
+export default TagAvatars;
