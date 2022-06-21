a49d3040c533fe0f7b12a0510c10edee24d3d6bd
feel0321 <dishate@naver.com>
2022-06-21 23:03:56 +0900

    feat: 메시지 페이지의 초기 로딩에 보여줄 SkeletonMesasge를 추가했다

---
+++ src/components/SkeletonMessage/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import Skeleton from '@mui/material/Skeleton';
+
+const SkeletonContainer = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  border-radius: 8px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  padding: 1rem;
+  display: flex;
+  gap: 1rem;
+`;
+
+const SkeletonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex-grow: 1;
+`;
+
+function SkeletonMessage({ repeat }) {
+  const repeatArr = Array.from({ length: repeat }, (_, index) => index);
+
+  return repeatArr.map((num) => (
+    <SkeletonContainer key={num}>
+      <Skeleton variant="circular" width={40} height={40} />
+      <SkeletonWrapper>
+        <Skeleton variant="text" width="33%" height="50%" />
+        <Skeleton variant="text" height="50%" />
+      </SkeletonWrapper>
+    </SkeletonContainer>
+  ));
+}
+
+export default SkeletonMessage;
--- src/pages/MessagePage.js
+++ src/pages/MessagePage.js
@@ -4,34 +4,38 @@ import useValueContext from '@hooks/useValueContext';
 import { useNavigate } from 'react-router-dom';
 import useInterval from '@hooks/useInterval';
 import Card from '@components/Card';
+import SkeletonMessage from '@components/SkeletonMessage';
 
 function MessagePage() {
   const [myMessageList, setMyMessageList] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { user } = useValueContext();
   const navigate = useNavigate();
 
   const handleClickGetMessageButton = useCallback(() => {
     console.log(123);
-    getMyMessageList().then((response) => setMyMessageList(response));
+    getMyMessageList().then((response) => {
+      setMyMessageList(response);
+      setLoading(false);
+    });
   }, []);
 
   useInterval(handleClickGetMessageButton, 3000);
 
-  return (
-    <div>
-      {myMessageList.length && user
-        ? myMessageList.map(({ message, receiver, sender }) => {
-            const you = receiver._id !== user._id ? receiver : sender;
-            const { _id } = you;
-            return (
-              <Card.User key={_id} onClick={() => navigate(`/message/${_id}`)}>
-                {{ ...you, email: message }}
-              </Card.User>
-            );
-          })
-        : '아직 대화한 상대가 없어요'}
-    </div>
-  );
+  const Loaded =
+    myMessageList.length && user
+      ? myMessageList.map(({ message, receiver, sender }) => {
+          const you = receiver._id !== user._id ? receiver : sender;
+          const { _id } = you;
+          return (
+            <Card.User key={_id} onClick={() => navigate(`/message/${_id}`)}>
+              {{ ...you, email: message }}
+            </Card.User>
+          );
+        })
+      : '아직 대화한 상대가 없어요';
+
+  return <div>{loading ? <SkeletonMessage repeat={5} /> : Loaded}</div>;
 }
 
 export default MessagePage;
