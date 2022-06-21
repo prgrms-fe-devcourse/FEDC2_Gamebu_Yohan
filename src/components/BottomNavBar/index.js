0a469926cf55e28f0c5a5b957848e34243276428
feel0321 <dishate@naver.com>
2022-06-21 21:42:15 +0900

    feat: BottomNavbar의 메시지 아이콘을 수정했다, 메시지 클릭 시 로그인 여부를 검사하도록 추가했다

--- src/components/BottomNavBar/index.js
+++ src/components/BottomNavBar/index.js
@@ -1,20 +1,36 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import styled from '@emotion/styled';
 import HomeIcon from '@mui/icons-material/Home';
 import WidgetsIcon from '@mui/icons-material/Widgets';
 import SearchIcon from '@mui/icons-material/Search';
+import SendIcon from '@mui/icons-material/Send';
 import { useNavigate, useLocation } from 'react-router-dom';
+import useValueContext from '@hooks/useValueContext';
+import LoginModal from '@components/LoginModal';
 
 const BottomNavbarWrapper = styled.div`
   width: 100%;
 `;
 
+const routingTable = {
+  0: '/',
+  1: '/channel/categories',
+  2: '/search/all',
+  3: '/message',
+};
+
 function BottomNavBar() {
   const [value, setValue] = useState(0);
+  const [visible, setVisible] = useState(false);
   const { pathname } = useLocation();
   const navigate = useNavigate();
+  const { isLogin } = useValueContext();
+
+  const handleCloseModal = useCallback(() => {
+    setVisible(false);
+  }, []);
 
   useEffect(() => {
     if (pathname.length === 1) {
@@ -32,12 +48,6 @@ function BottomNavBar() {
     return setValue(1);
   }, [pathname]);
 
-  const routingTable = {
-    0: '/',
-    1: '/channel/categories',
-    2: '/search/all',
-    3: '/message',
-  };
   return (
     <BottomNavbarWrapper>
       <BottomNavigation
@@ -45,6 +55,10 @@ function BottomNavBar() {
         value={value}
         onChange={(_, newValue) => {
           if (newValue === value) return;
+          if (newValue === 3 && !isLogin) {
+            setVisible(true);
+            return;
+          }
           setValue(newValue);
           navigate(routingTable[newValue]);
         }}
@@ -52,8 +66,9 @@ function BottomNavBar() {
         <BottomNavigationAction label="홈" icon={<HomeIcon />} />
         <BottomNavigationAction label="채널" icon={<WidgetsIcon />} />
         <BottomNavigationAction label="검색" icon={<SearchIcon />} />
-        <BottomNavigationAction label="메시지" icon={<SearchIcon />} />
+        <BottomNavigationAction label="메시지" icon={<SendIcon />} />
       </BottomNavigation>
+      <LoginModal visible={visible} handleCloseModal={handleCloseModal} />
     </BottomNavbarWrapper>
   );
 }
