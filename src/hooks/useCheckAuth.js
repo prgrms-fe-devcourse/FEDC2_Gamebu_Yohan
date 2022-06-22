6fe6ebb9226b5d1a80226a92544a3b84a436b0fa
feel0321 <dishate@naver.com>
2022-06-22 18:39:57 +0900

    feat: 비로그인 유저가 페이지 접근 시 튕겨내는 useCheckAuth에 튕겨낼 때 snackbar를 추가했다

--- src/hooks/useCheckAuth.js
+++ src/hooks/useCheckAuth.js
@@ -1,16 +1,19 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import useOurSnackbar from './useOurSnackbar';
 import useValueContext from './useValueContext';
 
 const useCheckAuth = () => {
   const { initialLoading, isLogin } = useValueContext();
   const navigate = useNavigate();
+  const renderSnackbar = useOurSnackbar();
 
   useEffect(() => {
     if (!isLogin && !initialLoading) {
+      renderSnackbar('로그인 유저만 접속 가능한 페이지입니다');
       navigate('/');
     }
-  }, [initialLoading, isLogin, navigate]);
+  }, [initialLoading, isLogin, navigate, renderSnackbar]);
 };
 
 export default useCheckAuth;
